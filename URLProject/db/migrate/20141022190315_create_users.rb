82a3206477801966bb3890959ad0bc3bfa733846
Adam Berman <adamberman13@gmail.com>
2014-10-22 13:27:50 -0700

    create user model

--- URLProject/db/migrate/20141022190315_create_users.rb
+++ URLProject/db/migrate/20141022190315_create_users.rb
@@ -4,6 +4,15 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email, :null => false
       
       t.timestamps
+      
+      
     end
+  end
+  
+end
+
+class AddIndexes < ActiveRecord::Migration
+  def change
+    add_index(:users, :email, unique: true)
   end
 end
