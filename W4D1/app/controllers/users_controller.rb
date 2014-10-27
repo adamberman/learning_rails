fed88c47f020a3cef62da3bb25e8afec2f020cfd
App Academy Student <student@appacademy.io>
2014-10-27 13:59:10 -0700

    made controller stuff

--- W4D1/app/controllers/users_controller.rb
+++ W4D1/app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def index
-    render text: "I'm in the index action!"
+    @users = User.all
+    render json: @users
+  end
+  
+  def create
+    user = User.new(params[:user].permit(:name, :email))
+    if user.save
+      render json: user
+    else
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 end
--- W4D1/bin/my_script.rb
+++ W4D1/bin/my_script.rb
@@ -1,11 +1,28 @@
 require 'addressable/uri'
 require 'rest-client'
 
-url = Addressable::URI.new(
-  scheme: 'http',
-  host: 'localhost',
-  port: 3000,
-  path: '/users.html'
-).to_s
+def show_user(id)
 
-puts RestClient.get(url)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: "/users/#{id}"
+  ).to_s
+
+  puts RestClient.get(url)
+end
+
+def create_user(name, email)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(
+    url,
+    { user: { name: name, email: email } }
+  )
+end
