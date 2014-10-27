77e13b08c464f6dfdc2d7b1aa4d7bc042041c491
adamberman <adamberman13@gmail.com>
2014-10-26 19:39:36 -0700

    add flash and debug the delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,13 +16,15 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
     	@article.save
+    	flash.notice = "Article '#{@article.title}' Created!"
     	redirect_to article_path(@article)
 	end
 
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
-		redirect_to index_path
+		flash.notice = "Article '#{@article.title}' Destroyed!"
+		redirect_to articles_path
 	end
 
 	def edit
@@ -32,6 +34,7 @@ class ArticlesController < ApplicationController
 	def update
 		@article = Article.find(params[:id])
 		@article.update(article_params)
+		flash.notice = "Article '#{@article.title}' Updated!"
 		redirect_to article_path(@article)
 	end
 end
