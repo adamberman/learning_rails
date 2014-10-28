82289aac59d9b5dc979d495dab6a565a0b220a0c
App Academy Student <student@appacademy.io>
2014-10-28 10:52:52 -0700

    create cat model

---
+++ NinetyNineCats/app/models/cat.rb
@@ -0,0 +1,18 @@
+class Cat < ActiveRecord::Base
+  validate :timeliness
+  validates :color, inclusion: { in: %w[black brown calico orange],
+            message: "Cats don't come in that color" }
+  validates :name, :birth_date, :color, :sex, presence: true
+  validates :color, inclusion: { in: %w[M F], message: "Sex must be M or F" }
+  
+  def age
+    ((Date::today - birth_date) / 365).to_i
+  end
+  private
+  
+  def timeliness
+    if birth_date > Date::today
+      errors[:days] << "Birthday cannot be after today"
+    end
+  end
+end
