c25a8ca928b3652d24f95c615db1269551b68e8e
adamberman <adamberman13@gmail.com>
2014-10-30 14:51:14 -0700

    write bands controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,3 +1,29 @@
 class BandsController < ApplicationController
-	
+	def new
+		@band = Band.new
+		render :new
+	end
+
+	def create
+		@band = Band.new(band_params)
+		if @band.save
+			redirect_to band_url(@band)
+		else
+			render new
+		end
+	end
+
+	def show
+		@band = Band.find(params[:id])
+		render :show
+	end
+
+	def index
+		@bands = Band.all
+		render :index
+	end
+
+	def destroy
+		Band.find(params[:id]).destroy
+	end
 end
