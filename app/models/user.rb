2442a8300427b311d117714cd5ddf75d35ad4a4e
adamberman <adamberman13@gmail.com>
2014-10-30 11:32:44 -0700

    create session token and find_by_credentials methods" " "

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ActiveRecord::Base
 	validates :password, length { minimum: 6, allow_nil: true }
 	after_initialize :set_session_token
 
+	def self.generate_session_token
+		SecureRandom::urlsafe_base64(16)
+	end
+
+	def self.find_by_credentials(email, password)
+		user = User.find_by(email: email)
+		return nil unless user && user.is_password?(password)
+		user
+	end
+
+	def reset_session_token!
+		self.session_token = User.generate_session_token
+		self.save!
+		self.session_token
+	end
+
 	def password=(password)
 		@password = password
 		self.password_digest = BCrypt::Password.create(password)
@@ -18,6 +34,6 @@ class User < ActiveRecord::Base
 	private
 
 	def set_session_token
-		self.session_token ||= SecureRandom::urlsafe_base64(16)
+		self.session_token ||= User.generate_session_token
 	end
 end
