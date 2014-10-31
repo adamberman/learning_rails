3440707c01075964b816a42099119f21092e1564
adamberman <adamberman13@gmail.com>
2014-10-30 17:40:47 -0700

    make working new page for tracks

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -9,6 +9,7 @@ class AlbumsController < ApplicationController
 
 	def show
 		@album = Album.find(params[:id])
+		@tracks = @album.tracks
 		render :show
 	end
 
--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,8 @@ class Track < ActiveRecord::Base
 	validates :name, :album, :status, presence: true
 
 	belongs_to :album
+
+	def album_name
+		album.name
+	end
 end
